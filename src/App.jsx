1aa43810088872ad19064d294dcef21b0e4957df
Devasya <devasyarajguru@gmail.com>
2025-02-02 19:04:50 +0530

    feat: Rendering Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+// import { ThemeProvider } from "./context/ThemeContext" // Dark mode
+import { CurrencyProvider } from "./context/CurrencyContext" // manages currency selection
+import "./App.css"
+// import { CategoryProvider } from "./context/CategoryContext"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [wishlist, setWishlist] = useState([]) // store the llist of favorite products
+
+  useEffect(() => {
+    const savedWishlist = localStorage.getItem("wishlist") // retrieves wishlist data(String format) from localstorage
+
+    // if wishlist exists
+    if (savedWishlist) {
+      setWishlist(JSON.parse(savedWishlist)) // updating the state with stored items (JSON.parse converts string into Javascript array to use it with React's state)
+    }
+  }, [])
+
+  // Adding to wishlist
+  const addToWishlist = (product) => {
+    const updatedWishlist = [...wishlist, product] // Keeping the existing product and adding new product
+    setWishlist(updatedWishlist)
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist)) // Storing the data in localstorage 
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      {/* <ThemeProvider> */}
+        <CurrencyProvider>
+          {/* <CategoryProvider> */}
+          <div className="app">
+            <Header />
+            <Routes>
+            <Route path="/" element={<HomePage addToWishlist={addToWishlist} />} /> 
+            <Route path="/product/:id" element={<ProductPage addToWishlist={addToWishlist} />} /> 
+            </Routes>
+            <Footer />
+          </div>
+          {/* </CategoryProvider> */}
+        </CurrencyProvider>
+      {/* </ThemeProvider> */}
+    </Router>
   )
 }
 
 export default App
+
